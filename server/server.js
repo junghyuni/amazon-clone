62c21b43bbb6d85c9c747cb964d57a32ed52e9a2
junghyuni <anjjang23@gmail.com>
2020-12-22 11:32:10 +0900

    backend

--- server/server.js
+++ server/server.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/user')
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(
+  process.env.DATABASE, 
+  { useUnifiedTopology: true, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to the database")
+    }
+  }
+)
 
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// GET - retrieve data from the server
-app.get('/', (req, res) => {
-  res.json('hello amazon');
-});
+// require apis
+const productRoutes = require('./routes/product');
+const categoryRoutes = require('./routes/category');
+const ownerRoutes = require('./routes/owner');
 
-// POST - send data from frontend to backend
-app.post('/', (req, res) => {
-  console.log(req.body.name);
-});
+app.use('/api', productRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', ownerRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
